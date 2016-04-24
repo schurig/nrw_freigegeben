b7cf4588f80081176203d9e3e8f7c3fc3aa08869
Martin Schurig <martin@schurig.pw>
2016-04-24 14:39:58 +0200

    fix

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var Twitter = require('twitter');
 
+// var client = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+// });
+
 var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 });
 
 client.stream('user', {track: 'Freigabe,freigegeben'}, function(stream) {
@@ -12,10 +19,11 @@ client.stream('user', {track: 'Freigabe,freigegeben'}, function(stream) {
     if(tweet.user) {
       console.log('spottet tweet:');
       console.log(tweet.text);
-      if(tweet.user.id == 163576129) {
+      if(tweet.user.id == 163576129 || tweet.user.id == 3064346229) {
         // @regio_nrw: 163576129
+        // the last one is for testing it's all supposed tested by
         if(tweet.text.match(/freigabe|freigegeben/gi)) { // just to be sure..
-          if(tweet.text.match(/ice|ic|ec/gi)) {
+          if(tweet.text.match(/#ice|#ic|#ec/gi)) {
             retweetTweet(tweet.id_str);
           }
         }
@@ -36,3 +44,16 @@ retweetTweet = function retweetTweet(tweetId) {
     }
   });
 };
+
+
+// client.stream('statuses/filter', {track: '@martinschurig'}, function(stream) {
+//   stream.on('data', function(tweet) {
+//     console.log('spottet tweet:');
+//     console.log(tweet.text);
+//     retweetTweet(tweet.id_str);
+//   });
+
+//   stream.on('error', function(error) {
+//     throw error;
+//   });
+// });
